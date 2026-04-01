0546936a5e51de943d88c351e070642235398096
TQK work <tranquockhanh.work@gmail.com>
2026-04-01 22:00:21 +0700

    update

--- test_fee10.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-$app = require_once __DIR__ . '/bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-$vtp = app(App\Services\ViettelPostService::class);
-$fees = $vtp->calculateFee(5, 91, 1, 1);
-var_dump($fees);
