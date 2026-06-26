d315ba65f48d0463909df55c47fdfd932c485f9f
leo.tran <leo.tran@beyondedge.co>
2026-06-26 23:22:57 +0700

    Polish application maintenance updates

--- test_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-$app = require_once __DIR__ . '/bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-$senderProvince = (int) config('services.viettelpost.sender_province', 5);
-$senderDistrict = (int) config('services.viettelpost.sender_district', 3591);
-
-echo "Config province: $senderProvince, district: $senderDistrict\n";
--- test_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-$app = require_once __DIR__ . '/bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-foreach (App\Models\CustomerAddress::all() as $a) {
-    echo "ID: {$a->id}, Prov: {$a->province_id}, Dist: {$a->district_id}\n";
-}
